7fcd1bdc9a022869af8e1d80332ece37b67ef86b
DanielEverett1 <kathpromotions@gmail.com>
2023-04-17 21:43:44 -0400

    Updated

--- signUp.js
+++ signUp.js
@@ -1,7 +1,11 @@
+const { createClient } = require('@supabase/supabase-js')
 const { PrismaClient } = require('@prisma/client')
-const prompt = require('prompt-sync')();
+// const prompt = require('prompt-sync')();
 const prisma = new PrismaClient()
+require.dotenv()
 
+// Create a single supabase client for interacting with your database
+const supabase = createClient(`${DATABASE_URL}`, `${publicAnonKey}`)
 
 let name = prompt("Please enter your name: ")
 let email = prompt("Please enter an email: ")
@@ -34,12 +38,7 @@ async function main() {
 
     console.dir(allUsers, { depth: null })
 
-    //   const post = await prisma.post.update({
-    //     where: { id: 1 },
-    //     data: { published: true },
-    //   })
-
-    //   console.log(post)
+    // await prisma.user.deleteMany({})
 }
 
 main()
@@ -50,4 +49,22 @@ main()
         console.error(e)
         await prisma.$disconnect()
         process.exit(1)
-})
+     })
+
+// async function signUp() {
+//     const { data, error } = await supabase.auth.signUp({
+//         email: 'example@email.com',
+//         password: 'example-password',
+//     })
+// }
+
+// async function signIn() {
+//     const { data, error } = await supabase.auth.signInWithPassword({
+//         email: 'example@email.com',
+//         password: 'example-password',
+//     })
+// }
+
+// async function signOut() {
+//     const { error } = await supabase.auth.signOut()
+// }
